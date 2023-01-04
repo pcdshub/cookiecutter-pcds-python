4524abde9124fbb4f3a232615743fa4882b9ce21
Ken Lauer <klauer@slac.stanford.edu>
2023-01-04 11:18:10 -0800

    MNT: note about tests

--- {{ cookiecutter.folder_name }}/{{ cookiecutter.import_name }}/tests/test_blank.py
+++ {{ cookiecutter.folder_name }}/{{ cookiecutter.import_name }}/tests/test_blank.py
@@ -1,7 +1,8 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-
 def test_blank():
+    """
+    This is a placeholder for some testing and to verify your continuous
+    integration works as expected.
+
+    Remove this function and write your own tests!
+    """
     raise ZeroDivisionError()
